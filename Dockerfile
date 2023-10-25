db3585c03f695c9e73ddd4eb79fe6c31a4de60af
Artem Korobov <artyom.korobov51@gmail.com>
2023-10-25 15:04:33 +0300

    Postges connect to Django

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
-FROM python:3.11
+FROM python:3.9-alpine3.16
 
 COPY requirements.txt /temp/requirements.txt
 COPY service /service
 WORKDIR /service
 EXPOSE 8000
+
 
 RUN pip install -r /temp/requirements.txt
 
--- service/service/settings.py
+++ service/service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure--2smzj+8j=%uv*-jg#4@^lx+ypqi-0+w#l0e=hnwz6nt#@0f_-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'PORT': '5432',
     }
 }
 
