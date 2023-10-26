e88ab8bc111aee28913c9f24a291b8337876e354
Artem Korobov <artyom.korobov51@gmail.com>
2023-10-26 13:41:04 +0300

    Send mail logic with Gmail

--- service/service/settings.py
+++ service/service/settings.py
@@ -129,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'artyom.korobov51@gmail.com'
+EMAIL_HOST_PASSWORD = 'secretpassword'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'default from email'
--- service/services/views.py
+++ service/services/views.py
@@ -1,9 +1,22 @@
+from django.core.mail import send_mail
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from sportsman.models import Sportsman_user
 from services.serializers import AthleteSerializer
 
+
 class AthleteInfoView(ReadOnlyModelViewSet):
     queryset = Sportsman_user.objects.all()
     serializer_class = AthleteSerializer
+
+
+def send_email(request):
+    send_mail(
+        subject='Подтвердите регистрацию',
+        message='Пройдите по ссылкке',
+        from_email='artyom.korobov51@gmail.com',
+        recipient_list=['projectmaildjango344@gmail.com']
+    )
+    return HttpResponse('ok')
